7b7ac1d60dfcf7812af8e81e3ab700dd684399d8
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-07-29 17:09:37 +0300

    Add garbage collector implementation
    Introduces gc_malloc, gc_add, and gc_clear functions for memory management. Allocated pointers are tracked and freed collectively to simplify resource cleanup.

---
+++ gc/gc.c
@@ -0,0 +1,37 @@
+#include "gc.h"
+
+void	*gc_malloc(t_gc *gc, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	gc_add(gc, ptr);
+	return (ptr);
+}
+
+void	gc_add(t_gc *gc, void *ptr)
+{
+	t_alloc	*new;
+
+	new = malloc(sizeof(t_alloc));
+	if (!new)
+		return ;
+	new->ptr = ptr;
+	new->next = gc->head;
+	gc->head = new;
+}
+
+void	gc_clear(t_gc *gc)
+{
+	t_alloc	*tmp;
+
+	while (gc->head)
+	{
+		tmp = gc->head;
+		free(tmp->ptr);
+		gc->head = tmp->next;
+		free(tmp);
+	}
+}
