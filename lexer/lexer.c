69b089c1a1a76f694b98d99f5e175796176ce1e9
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-07-30 13:35:23 +0300

    Add lexer implementation for minishell

---
+++ lexer/lexer.c
@@ -0,0 +1,62 @@
+#include "minishell.h"
+
+static t_token	*create_token(t_minishell *sh, char *val, t_token_type type)
+{
+	t_token	*tok;
+
+	tok = gc_malloc(&sh->gc, sizeof(t_token));
+	tok->value = val;
+	tok->type = type;
+	tok->next = NULL;
+	return (tok);
+}
+
+static t_token *get_next_token(const char **s, t_minishell *sh)
+{
+    char			*word;
+    char			*op;
+    t_token_type	type;
+
+    if (**s == '\'' || **s == '"' || !ft_strchr("|&<>() ", **s))
+    {
+        word = extract_word(s, sh);
+        if (!word)
+            return (NULL);
+        return (create_token(sh, word, T_WORD));
+    }
+    op = extract_operator(s, &type, sh);
+    if (!op)
+        return (NULL);
+    return (create_token(sh, op, type));
+}
+
+static void	add_token_to_list(
+	t_token **head, t_token **tail, t_token *new_token)
+{
+	if (!*head)
+		*head = new_token;
+	else
+		(*tail)->next = new_token;
+	*tail = new_token;
+}
+
+t_token	*tokenize_input(const char *input, t_minishell *sh)
+{
+    t_token	*head;
+    t_token	*tail;
+    t_token	*new_token;
+
+    head = NULL;
+    tail = NULL;
+    while (*input)
+    {
+        skip_whitespace(&input);
+        if (!*input)
+            break ;
+        new_token = get_next_token(&input, sh);
+        if (!new_token)
+            return (NULL); // or handle error
+        add_token_to_list(&head, &tail, new_token);
+    }
+    return (head);
+}
---
+++ lexer/lexer_utils.c
@@ -0,0 +1,67 @@
+#include "minishell.h"
+
+void	skip_whitespace(const char **s)
+{
+	while (**s == ' ' || **s == '\t')
+		(*s)++;
+}
+
+static char	*copy_word(const char *start, size_t len, t_minishell *sh)
+{
+	char	*word;
+
+	word = gc_malloc(&sh->gc, len + 1);
+	ft_strncpy(word, start, len);
+	word[len] = '\0';
+	return (word);
+}
+
+char	*extract_word(const char **s, t_minishell *sh)
+{
+	const char	*start;
+	size_t		len;
+
+	start = *s;
+	while (**s && !ft_strchr(" \t|&<>()", **s))
+	{
+		if (**s == '\'' || **s == '"')
+			break ;
+		(*s)++;
+	}
+	len = *s - start;
+	return (copy_word(start, len, sh));
+}
+
+static char	*set_token_and_advance(const char **s, int len, t_minishell *sh)
+{
+	char	*op;
+
+	op = gc_malloc(&sh->gc, len + 1);
+	ft_memcpy(op, *s, len);
+	op[len] = '\0';
+	*s += len;
+	return (op);
+}
+
+char	*extract_operator(const char **s, t_token_type *type, t_minishell *sh)
+{
+	if (ft_strncmp(*s, "&&", 2) == 0)
+		return (*type = T_AND_IF, set_token_and_advance(s, 2, sh));
+	if (ft_strncmp(*s, "||", 2) == 0)
+		return (*type = T_OR_IF, set_token_and_advance(s, 2, sh));
+	if (ft_strncmp(*s, ">>", 2) == 0)
+		return (*type = T_REDIR_APPEND, set_token_and_advance(s, 2, sh));
+	if (ft_strncmp(*s, "<<", 2) == 0)
+		return (*type = T_HEREDOC, set_token_and_advance(s, 2, sh));
+	if (**s == '|')
+		return (*type = T_PIPE, set_token_and_advance(s, 1, sh));
+	if (**s == '<')
+		return (*type = T_REDIR_IN, set_token_and_advance(s, 1, sh));
+	if (**s == '>')
+		return (*type = T_REDIR_OUT, set_token_and_advance(s, 1, sh));
+	if (**s == '(')
+		return (*type = T_PAREN_L, set_token_and_advance(s, 1, sh));
+	if (**s == ')')
+		return (*type = T_PAREN_R, set_token_and_advance(s, 1, sh));
+	return (NULL);
+}
