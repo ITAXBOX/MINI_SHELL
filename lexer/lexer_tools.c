dc6f8a2b7cfdda3b63d33788dd5141b2371a5c30
ALI ITAWI <lenovoy520itaxbox@gmail.com>
2025-07-30 21:22:15 +0300

    Add variable value retrieval and exit status handling

--- lexer/lexer_tools.c
+++ lexer/lexer_tools.c
@@ -1,5 +1,34 @@
 #include "minishell.h"
 
+static char	*itoa_exit_status(int status, t_minishell *sh)
+{
+	char	buf[12];
+	int		i;
+
+	i = 10;
+	buf[11] = '\0';
+	if (status == 0)
+		buf[--i] = '0';
+	while (status > 0)
+	{
+		buf[--i] = '0' + (status % 10);
+		status /= 10;
+	}
+	return (gc_strdup(&buf[i], &sh->gc));
+}
+
+char	*get_var_value(const char *var, t_minishell *sh)
+{
+	char	*val;
+
+	if (ft_strncmp(var, "?", 1) == 0 && var[1] == '\0')
+		return (itoa_exit_status(sh->last_exit_status, sh));
+	val = getenv(var);
+	if (!val)
+		return (gc_strdup("", &sh->gc));
+	return (gc_strdup(val, &sh->gc));
+}
+
 char	*extract_quoted_word(const char **s, t_minishell *sh)
 {
 	char		quote;
