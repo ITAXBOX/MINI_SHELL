ec83b364f1d1aec2fca558f8028cbec1206f5ff3
mhmdam14 <mhmdam12.almhmd@gmail.com>
2025-08-08 19:42:46 +0300

    fixed variable expansions in heredoc when the delimiter is partially quoted

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -12,28 +12,40 @@ static t_token	*create_token(t_minishell *sh, char *val, t_token_type type)
 	return (tok);
 }
 
+static int	has_quotes_in_word(const char *s)
+{
+	while (*s && !ft_strchr("|&<>() \t", *s))
+	{
+		if (*s == '\'' || *s == '"')
+			return (1);
+		if (*s == '\\' && *(s + 1))
+			s++;
+		s++;
+	}
+	return (0);
+}
+
 static t_token	*get_next_token(const char **s, t_minishell *sh)
 {
 	char			*word;
-	char			*op;
 	t_token_type	type;
 	t_token			*token;
-	int				quoted;
+	const char		*start;
 
-	quoted = (**s == '\'' || **s == '"');
 	if (**s == '\'' || **s == '"' || !ft_strchr("|&<>() \t", **s))
 	{
+		start = *s;
 		word = extract_word(s, sh);
 		if (!word)
 			return (NULL);
 		token = create_token(sh, word, T_WORD);
-		token->was_quoted = quoted;
+		token->was_quoted = has_quotes_in_word(start);
 		return (token);
 	}
-	op = extract_operator(s, &type, sh);
-	if (!op)
+	word = extract_operator(s, &type, sh);
+	if (!word)
 		return (NULL);
-	return (create_token(sh, op, type));
+	return (create_token(sh, word, type));
 }
 
 static void	add_token_to_list(
